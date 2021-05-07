e8b7b2d82f1b770f8ce5a779192fbc40f9381d9e
Evgenii Puliaev <puliaevevgenii@gmail.com>
2021-05-07 13:45:20 +0300

    msg

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   outputDir: 'build/dist',
   assetsDir: 'static',
-  /*devServer: {
-    port: 4444,
-    proxy: {
-      "^/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-      },
-    },
-  },*/
+  devServer: {
+    host: 'localhost',
+    port: 8081,
+//    proxy: {
+//      "^/api": {
+//        target: "http://localhost:8080",
+//        changeOrigin: true,
+//      },
+//    },
+  },
   transpileDependencies: ["vuetify"],
 };
